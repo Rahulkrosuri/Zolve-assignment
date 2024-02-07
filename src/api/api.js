8d55a0f49d7f1d123100c62ea2ca7326d9e1435f
Rahulkrosuri <rahulkrosuri@gmail.com>
2024-02-07 07:42:44 +0530

    date to unix timestamp

--- src/api/api.js
+++ src/api/api.js
@@ -3,8 +3,8 @@ import axios from 'axios'
 const apiCall = async (data) => {
     let pagesize = data.pagesize
     let page = data.page
-    let fromDate = data.fromDate
-    let toDate = data.toDate
+    let fromDate = new Date(data.fromDate).getTime()/1000
+    let toDate = new Date(data.toDate).getTime()/1000
     const url = `https://api.stackexchange.com/2.3/tags?page=${page}&pagesize=${pagesize}&fromdate=${fromDate}&todate=${toDate}&order=desc&sort=popular&site=stackoverflow`
     let response = await axios.get(url)
     if (response) return response.data
